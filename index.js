cc8f4d769637fc0d2dd579b26907f919a10a1e38
Karina Hrama <hrama.karina@chnu.edu.ua>
2023-05-29 23:07:37 +0300

    added PULL_REQUEST_TEMPLATE.md

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ bot.on('callback_query', async (query) => {
 			      onMyLocation(id, bot, query, store)
 				break;
 
-
+//commit
 				//show_near_not
 		//повернення на головну сторінку
 		case mode_app.return_home:
